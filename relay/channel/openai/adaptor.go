c4acfe0acc259964f42b8848813ed3a3d236bd9c
yankai1221 <yankai1221@gmail.com>
2026-04-20 00:33:00 +0800

    更新log

--- relay/channel/openai/adaptor.go
+++ relay/channel/openai/adaptor.go
@@ -231,10 +231,10 @@ func (a *Adaptor) SetupRequestHeader(c *gin.Context, header *http.Header, info *
 	}
 	if info.ChannelType == constant.ChannelTypeOpenRouter {
 		if header.Get("HTTP-Referer") == "" {
-			header.Set("HTTP-Referer", "https://www.newapi.ai")
+			header.Set("HTTP-Referer", "https://zhumuai.com")
 		}
 		if header.Get("X-OpenRouter-Title") == "" {
-			header.Set("X-OpenRouter-Title", "New API")
+			header.Set("X-OpenRouter-Title", "瞩目AI")
 		}
 	}
 	return nil
